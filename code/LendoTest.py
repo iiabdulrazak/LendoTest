eb1ce7332432beeaeb9c99cd8877ef1ba0487c8f
Abdulrazak Osman <zaak0101@hotmail.com>
2021-05-06 15:00:06 +0300

    Solution: Inserting Into the databse

--- code/LendoTest.py
+++ code/LendoTest.py
@@ -1,12 +1,7 @@
 import MySQLdb
 import csv
 import sys
-import configuration
-
-host="sql6.freesqldatabase.com"
-user="sql6410213"
-password="GdeWcDcBxd"
-database="sql6410213"
+import config
 
 conn = MySQLdb.connect(host=host, user=user, password=password, database=database)
 
@@ -23,3 +18,23 @@ for row in csv_data:
 conn.commit()
 cursor.close()
 print('Done')
+
+'''
+import sqlalchemy
+import pandas as pd
+
+
+#creating engine to be able of connecting to the database,
+#we gonna use 'sqlalchemy' to build the engine and 'mysql' as my database,
+#the structure would be: ‘mysql://username:password@databasehost:port/databasename’
+
+# Create the engine to connect to the MySQL database
+engine = sqlalchemy.create_engine('mysql://sql6410213:GdeWcDcBxd@sql6.freesqldatabase.com:3306/sql6410213')
+# Read data from CSV and load into a dataframe object
+data = pd.read_csv('../data/covidCases.csv')
+# Write data into the table in MySQL database
+data.to_sql('corona-test',engine)
+'''
+
+#first method is the slowest because of the looping process,
+#second method is the fasts because of the optimaization using pyPackage
